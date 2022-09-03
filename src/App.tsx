452e2b548e4c9b2df87ff922ad688da49d1a14d7
Ebium <erwan.boukerche@gmail.com>
2022-09-03 14:25:08 +0200

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react"
+import styled from "styled-components"
 
 export const App = () => {
+  interface OneTabElement {
+    label: number
+    value: number
+  }
+
+  const boardArrayPretab = Array<OneTabElement>(100).fill({
+    value: 0,
+    label: 0,
+  })
+  const [boardArray, setboardArray] = useState<OneTabElement[]>(
+    boardArrayPretab.map((key, index) => {
+      return { label: index + 1, value: 0 }
+    })
+  )
+  console.log(boardArray)
 
   const Cellule2 = () => {
-    return (
-      <Cellule>
-      </Cellule>
-    )
+    return <Cellule></Cellule>
   }
 
   return (
     <Content>
       <CenterContent>
-        <Cellule2 />
+        {boardArray.map((key, index) => (
+          <Cellule2 />
+          
+        ))}
       </CenterContent>
     </Content>
-  );
-};
-
+  )
+}
 
 const Content = styled.div`
   width: 100vw;
   height: 100vh;
-`;
+`
 
 const CenterContent = styled.div`
   display: flex;
@@ -36,8 +50,17 @@ const CenterContent = styled.div`
 `
 
 const Cellule = styled.div`
-background-color: grey;
-width: 2rem;
-height: 2rem;
-margin: 0.5rem;
+  background-color: grey;
+  width: 2rem;
+  height: 2rem;
+  margin: 0.5rem;
+`
+
+const RowStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+const ColumnStyle = styled.div`
+  display: flex;
+  flex-direction: column;
 `
