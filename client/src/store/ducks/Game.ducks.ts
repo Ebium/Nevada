9c3f3107853c3bb216d6c4c01c49b24a99742dfb
cyril <cyril@pop-os.localdomain>
2022-11-25 20:15:27 +0100

    fix conflicts

--- client/src/store/ducks/Game.ducks.ts
+++ client/src/store/ducks/Game.ducks.ts
@@ -1,20 +1,39 @@
+import { Move } from "../../utils/Moves"
 export const enum GameActionsEnum {
   UPDATE_GAME_CAN_START = "GAME/updateGameCanStart",
+
+  UPDATE_MOVES_HISTORY = "GAME/updateMovesHistory",
 }
 
 export const updateGameCanStart = (bool: boolean) =>
-  ({ type: GameActionsEnum.UPDATE_GAME_CAN_START, bool } as const)
+({
+  type: GameActionsEnum.UPDATE_GAME_CAN_START,
+  bool
+} as const)
+export const updateMovesHistory = (moves: Move[], count: number) =>
+({
+  type: GameActionsEnum.UPDATE_MOVES_HISTORY,
+  moves,
+  count
+} as const)
 
-type GameActionsType = ReturnType<typeof updateGameCanStart>
+type GameActionsType = ReturnType<
+  | typeof updateGameCanStart
+  | typeof updateMovesHistory
+>
 
 export interface GameState {
   started: boolean
   status: "idle" | "loading" | "failed"
+  movesHistory: Move[]
+  movesCount: number
 }
 
 export const gameInitialState: GameState = {
   started: false,
   status: "idle",
+  movesHistory: [],
+  movesCount: 0,
 }
 
 export function GameReducer(
@@ -24,6 +43,8 @@ export function GameReducer(
   switch (action.type) {
     case GameActionsEnum.UPDATE_GAME_CAN_START:
       return { ...state, started: action.bool }
+    case GameActionsEnum.UPDATE_MOVES_HISTORY:
+      return { ...state, movesHistory: action.moves, movesCount: action.count }
     default:
       return { ...state }
   }
