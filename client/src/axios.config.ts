0262345aaa394351b038de3308cbdb695439f740
Erwan Boukerche <erwanboukerche@pop-os.localdomain>
2023-01-12 12:34:02 +0100

    pullup moi or cons

--- client/src/axios.config.ts
+++ client/src/axios.config.ts
@@ -3,8 +3,4 @@ import axios from "axios"
 
 export const ConfigureAxios = () => { 
     axios.defaults.baseURL = configLocal.baseURL
-    axios.defaults.withCredentials = true
-    axios.defaults.maxRedirects = 0
-    axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*"
-    axios.defaults.headers.put["Access-Control-Allow-Origin"] = "*"
 }
--- client/src/config.ts
+++ client/src/config.ts
@@ -3,9 +3,9 @@ interface Config {
 }
 
 export const configyProduction: Config = {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:5050",
 }
 
 export const configLocal: Config = {
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:5050",
 }
